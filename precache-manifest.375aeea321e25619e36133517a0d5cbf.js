2b33cd33c2026273969952ff37d77564053b6503
Conor <chunter668@hotmail.com>
2020-09-21 21:06:55 +0100

    Updates

---
+++ precache-manifest.375aeea321e25619e36133517a0d5cbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbe53f02c7b3f8fc8cf8",
+    "url": "/portfolio/static/css/main.c6fefa7a.chunk.css"
+  },
+  {
+    "revision": "cbe53f02c7b3f8fc8cf8",
+    "url": "/portfolio/static/js/main.cbe53f02.chunk.js"
+  },
+  {
+    "revision": "c0106119511758264dda",
+    "url": "/portfolio/static/js/1.c0106119.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "86870ac582392e605ce819d070da4341",
+    "url": "/portfolio/index.html"
+  }
+];
